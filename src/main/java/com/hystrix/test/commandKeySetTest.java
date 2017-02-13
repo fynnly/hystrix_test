c8466c00aab574080b4aa27ca4ee396f04a3a5b5
libin18 <libin18@meituan.com>
2017-02-13 14:57:36 +0800

    add demo

--- src/main/java/com/hystrix/test/commandKeySetTest.java
+++ src/main/java/com/hystrix/test/commandKeySetTest.java
@@ -1,8 +1,6 @@
 package com.hystrix.test;
 
 import com.hystrix.test.common.BaseTest;
-import com.hystrix.test.util.CommonThreadPool;
-import com.hystrix.test.util.IThreadWork;
 import com.hystrix.test.util.SpringUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
@@ -10,14 +8,13 @@ import com.netflix.hystrix.contrib.javanica.conf.HystrixPropertiesManager;
 import org.junit.Test;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.Future;
-
 /**
- * 异步使用方式
+ * 测试同一个commandKey下,哪个配置生效
+ * 结论是先启动的配置生效
  */
 
 @Component
-public class AsyncFutureTest extends BaseTest{
+public class CommandKeySetTest extends BaseTest{
 
     @HystrixCommand(groupKey = "asyncFutureTest", commandKey = "asyncFutureTestCommandKey", fallbackMethod = "startFallback",
             commandProperties = {
@@ -40,14 +37,13 @@ public class AsyncFutureTest extends BaseTest{
     @HystrixCommand(groupKey = "asyncFutureTest", commandKey = "asyncFutureTestCommandKey", fallbackMethod = "getFallback",
             commandProperties = {
                     @HystrixProperty(name = HystrixPropertiesManager.EXECUTION_ISOLATION_STRATEGY, value = "SEMAPHORE"),
-                    @HystrixProperty(name = HystrixPropertiesManager.EXECUTION_ISOLATION_THREAD_TIMEOUT_IN_MILLISECONDS, value = "300"),
-                    @HystrixProperty(name = HystrixPropertiesManager.CIRCUIT_BREAKER_REQUEST_VOLUME_THRESHOLD, value = "3"),
-                    @HystrixProperty(name = HystrixPropertiesManager.CIRCUIT_BREAKER_ERROR_THRESHOLD_PERCENTAGE, value = "30"),
+                    @HystrixProperty(name = HystrixPropertiesManager.EXECUTION_ISOLATION_THREAD_TIMEOUT_IN_MILLISECONDS, value = "100"),
+                    @HystrixProperty(name = HystrixPropertiesManager.CIRCUIT_BREAKER_REQUEST_VOLUME_THRESHOLD, value = "2"),
+                    @HystrixProperty(name = HystrixPropertiesManager.CIRCUIT_BREAKER_ERROR_THRESHOLD_PERCENTAGE, value = "10"),
             }
     )
     public String get(){
         System.out.println("-------->get");
-        int i =1/0;
         return "success";
     }
     public String getFallback(){
@@ -57,39 +53,8 @@ public class AsyncFutureTest extends BaseTest{
 
     @Test
     public void test(){
-        final AsyncFutureTest instance = SpringUtil.getBean(AsyncFutureTest.class);
-        for (int i = 0;i<1;i++){
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            instance.start();
-            instance.get();
-        }
-//        CommonThreadPool threadPool = CommonThreadPool.getThreadPool();
-//        for (int i = 0;i<1;i++){
-//            try{
-//                Thread.sleep(1000);
-//                threadPool.addWork(new IThreadWork() {
-//                    @Override
-//                    public void doWork() {
-//                        instance.get();
-//                        instance.start();
-//                    }
-//                });
-//            }catch (Exception ex){
-//
-//            }
-//        }
-//        for (int i = 0;i<10;i++){
-//            instance.start();
-//        }
-
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-
-        }
+        final CommandKeySetTest instance = SpringUtil.getBean(CommandKeySetTest.class);
+        instance.start();
+        instance.get();
     }
 }
